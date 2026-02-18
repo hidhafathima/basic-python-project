09a79e329111f372b6b6a5fbf5647afe5bee1e5b
Hidha <hidha8888@gmail.com>
2026-02-19 04:51:49 +0530

    Improved ATM:added while loop for continous operation

--- atm_system.py
+++ atm_system.py
@@ -1,27 +1,29 @@
 print("Welcome to simple ATM System")
 balance=1000
-print("\nPlease choose an option:")
-print("1.Check Balance")
-print("2.Deposit Money")
-print("3.Withdraw Money")
-print("4.Exit")
-choice=input("Enter your choice (1-4): ")
-if choice=="1":
-    print("Your Current balance is :",balance)
-elif choice=="2":
-    deposit=float(input("Enter the amount to deposit"))
-    balance+=deposit
-    print("Deposit Successful")
-    print("Updated balance:",balance)
-elif choice=="3":
-    withdraw=float(input("Enter the amount to withdraw"))
-    if withdraw<=balance:
-        balance-=withdraw
-        print("Please collect your cash")
-        print("remaining balance:",balance)
-    else:
-        print("Insufficient Balance")
-elif choice=="4":
-    print("Thank You for using the ATM:Goodbye!")
-else:
-    print("Inavalid choice.Please try again.")
+while True:
+        print("\nPlease choose an option:")
+        print("1.Check Balance")
+        print("2.Deposit Money")
+        print("3.Withdraw Money")
+        print("4.Exit")
+        choice=input("Enter your choice (1-4): ")
+        if choice=="1":
+            print("Your Current balance is :",balance)
+        elif choice=="2":
+            deposit=float(input("Enter the amount to deposit"))
+            balance+=deposit
+            print("Deposit Successful")
+            print("Updated balance:",balance)
+        elif choice=="3":
+            withdraw=float(input("Enter the amount to withdraw"))
+            if withdraw<=balance:
+                balance-=withdraw
+                print("Please collect your cash")
+                print("remaining balance:",balance)
+            else:
+                print("Insufficient Balance")
+        elif choice=="4":
+            print("Thank You for using the ATM:Goodbye!")
+            break
+        else:
+            print("Inavalid choice.Please try again.")
